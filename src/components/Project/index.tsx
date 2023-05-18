4dcd1c1f4e57dc928b86f6657dd943ea0656dc69
Wagner <wavelino@gmail.com>
2023-05-18 17:46:04 -0300

    link github ok

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -16,7 +16,7 @@ interface ReposType {
   name: string;
   language: string;
   description: string;
-  git_url: string;
+  html_url: string;
   homepage: string;
 }
 
@@ -26,16 +26,16 @@ export const Project = (): JSX.Element => {
   useEffect(() => {
     const fetchData = async () => {
       const data: Response = await fetch(
-        `https://api.github.com/users/${userData.githubUser}/repos`
+        `https://api.github.com/users/${userData.githubUser}/repos?sort=created&direction=desc`
       )
 
       const json = await data.json();
 
       setRepositories(json);
 
-      if (!data.ok) {
-        throw data;
-      }
+      // if (!data.ok) {
+      //   throw data;
+      // }
 
       return json;
     };
@@ -44,37 +44,38 @@ export const Project = (): JSX.Element => {
 
   return (
     <>
-      {repositories?.map((repository) => (
-        <ProjectWrapper key={repository.id}>
-          <Text
-            as="h2"
-            type="heading3"
-            css={{ marginBottom: "$3" }}
-            color="grey1"
-          >
-            {repository.name}
-          </Text>
+      {repositories &&
+        repositories?.map((repository) => (
+          <ProjectWrapper key={repository.id}>
+            <Text
+              as="h2"
+              type="heading3"
+              css={{ marginBottom: "$3" }}
+              color="grey1"
+            >
+              {repository.name}
+            </Text>
 
-          {repository.language && (
-            <ProjectStack>
-              <Text type="body2">Linguagem:</Text>
-              <ProjectStackTech>
-                <Text color="brand1" type="body2">
-                  {repository.language}
-                </Text>
-              </ProjectStackTech>
-            </ProjectStack>
+            {repository.language && (
+              <ProjectStack>
+                <Text type="body2">Linguagem:</Text>
+                <ProjectStackTech>
+                  <Text color="brand1" type="body2">
+                    {repository.language}
+                  </Text>
+                </ProjectStackTech>
+              </ProjectStack>
           )}
 
           <Text type="body1" color="grey2">
-            {repository.description}
+            {repository.description?.substring(0, 129)}
           </Text>
           <ProjectLinks>
-            <ProjectLink target="_blank" href={repository.git_url}>
+            <ProjectLink target="_blank" href={repository.html_url}>
               <FaGithub /> Github Code
             </ProjectLink>
             {repository.homepage && (
-              <ProjectLink target="_blank" href={repository.homepage}>
+              <ProjectLink target="_blank" href={`https://${repository.homepage}`}>
                 <FaShare /> Aplicação
               </ProjectLink>
             )}
